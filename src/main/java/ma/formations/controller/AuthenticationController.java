92331939074f67d73e7661091042814319f4ced0
abbou <abbouformations@gmail.com>
2024-05-25 09:46:30 +0100

    add signup

--- src/main/java/ma/formations/controller/AuthenticationController.java
+++ src/main/java/ma/formations/controller/AuthenticationController.java
@@ -1,52 +1,61 @@
 package ma.formations.controller;
 
-import java.util.Collection;
-
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ma.formations.domaine.RoleVo;
+import ma.formations.domaine.TokenVo;
+import ma.formations.domaine.UserVo;
+import ma.formations.jwt.JwtUtils;
+import ma.formations.service.IUserService;
+import ma.formations.service.exception.BusinessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import ma.formations.domaine.TokenVo;
-import ma.formations.domaine.UserVo;
-import ma.formations.jwt.JwtUtils;
-import ma.formations.service.exception.BusinessException;
+import java.util.Collection;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-	private final AuthenticationManager authenticationManager;
-	private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+    private final IUserService userService;
 
-	@PostMapping("/signin")
-	public ResponseEntity<TokenVo> authenticateUser(@Valid @RequestBody UserVo userVo) {
+    @PostMapping("/signin")
+    public ResponseEntity<TokenVo> authenticateUser(@Valid @RequestBody UserVo userVo) {
 
-		try {
-			Authentication authentication = authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(userVo.getUsername(), userVo.getPassword()));
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-			String jwt = jwtUtils.generateJwtToken(authentication);
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(userVo.getUsername(), userVo.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.generateJwtToken(authentication);
 
-			TokenVo tokenVo = new TokenVo();
-			tokenVo.setJwttoken(jwt);
-			tokenVo.setUsername(userVo.getUsername());
-			Collection<? extends GrantedAuthority> list = authentication.getAuthorities();
-			list.forEach(authorite -> tokenVo.getRoles().add(authorite.getAuthority()));
-			return ResponseEntity.ok(tokenVo);
-		} catch (Exception e) {
-			throw new BusinessException("Login ou mot de passe incorrect");
-		}
+            TokenVo tokenVo = new TokenVo();
+            tokenVo.setJwttoken(jwt);
+            tokenVo.setUsername(userVo.getUsername());
+            Collection<? extends GrantedAuthority> list = authentication.getAuthorities();
+            list.forEach(authorite -> tokenVo.getRoles().add(authorite.getAuthority()));
+            return ResponseEntity.ok(tokenVo);
+        } catch (Exception e) {
+            throw new BusinessException("Login ou mot de passe incorrect");
+        }
 
-	}
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserVo userVo) {
+        if (userService.existsByUsername(userVo.getUsername())) {
+            return ResponseEntity.badRequest().body("Error: Username is already taken!");
+        }
+        // par défaut, le client a le rôle CLIENT
+        userVo.getRoles().add(new RoleVo("CLIENT"));
+        userService.save(userVo);
+        return ResponseEntity.ok("User registered successfully!");
+    }
 }
